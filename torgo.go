05564c583ae9b683f792c86563925d62eb84627f
Pedro Gabriel <pedrogabriel@protonmail.ch>
2020-01-21 22:00:08 +0100

    Reformat code

--- torgo.go
+++ torgo.go
@@ -16,29 +16,29 @@ func Parse(uri string) (error, *Torrent) {
 	ur := strings.Replace(uri, suf, "", len(suf))
 
 	parts := make(map[string]string)
-	urP  := strings.Split(ur, "&")
+	urP := strings.Split(ur, "&")
 	for _, p := range urP {
 		tmp := strings.Split(p, "=")
 		parts[tmp[0]] = tmp[1]
 	}
 
 	return nil, &Torrent{
-		Origin: uri,
-		Xt:parts["xt"],
-		DisplayName:parts["dn"],
-		Addr:parts["tr"],
+		Origin:      uri,
+		Xt:          parts["xt"],
+		DisplayName: parts["dn"],
+		Addr:        parts["tr"],
 	}
 }
 
 type Torrent struct {
-	Origin 		string
+	Origin      string
 	DisplayName string
 	Hash        string
 	Size        int64
-	Xt string
-	Addr string
+	Xt          string
+	Addr        string
 
-	Creation    float64
-	PieceLen    int
-	Pieces      int
+	Creation float64
+	PieceLen int
+	Pieces   int
 }
