10e656604568b25cd27083cae89f543dff3f6dbd
PeteGabriel <pedrogabriel@protonmail.ch>
2020-01-22 18:48:37 +0100

    Add more steps in the implementation of parse feature.

--- torgo.go
+++ torgo.go
@@ -2,8 +2,31 @@ package torgo
 
 import (
 	"errors"
+	"net/url"
 	"strings"
 )
+
+const (
+	DisplayName = "dn"
+	ExactTopic = "xt"
+	ExactSource = "xs"
+	Tracker = "tr"
+)
+
+type Torrent struct {
+	Origin      string   //original uri
+	DisplayName string   //dn
+	Hash        string   //info hash
+	Size        int64    // size in bytes
+	Xt          string   // exact topic
+	Trackers    []string //tr
+	Source      string
+
+	Creation float64
+	PieceLen int
+	Pieces   int
+}
+
 
 func Parse(uri string) (error, *Torrent) {
 
@@ -16,29 +39,40 @@ func Parse(uri string) (error, *Torrent) {
 	ur := strings.Replace(uri, suf, "", len(suf))
 
 	parts := make(map[string]string)
+	trs := make([]string, 0)
+	var btih string
+
 	urP := strings.Split(ur, "&")
 	for _, p := range urP {
 		tmp := strings.Split(p, "=")
-		parts[tmp[0]] = tmp[1]
+
+		if tmp[0] == Tracker {
+			trs = append(trs, decode(tmp[1]))
+		} else if tmp[0] == ExactTopic {
+			btih = strings.Replace(tmp[1], "urn:btih:", "", 1)
+			parts[ExactTopic] = decode(tmp[1])
+		} else {
+			parts[tmp[0]] = decode(tmp[1])
+		}
 	}
+
+	//parse TR links
 
 	return nil, &Torrent{
 		Origin:      uri,
-		Xt:          parts["xt"],
-		DisplayName: parts["dn"],
-		Addr:        parts["tr"],
+		Xt:          parts[ExactTopic],
+		DisplayName: parts[DisplayName],
+		Trackers:    trs,
+		Source:      parts[ExactSource],
+		Hash:        btih,
 	}
 }
 
-type Torrent struct {
-	Origin      string //original uri
-	DisplayName string //dn
-	Hash        string //info hash
-	Size        int64  // size in bytes
-	Xt          string // exact topic
-	Addr        string //tr
-
-	Creation float64
-	PieceLen int
-	Pieces   int
+func decode(src string) string {
+	if s, err := url.QueryUnescape(src); err != nil {
+		return ""
+	} else {
+		return s
+	}
 }
+
--- torgo_test.go
+++ torgo_test.go
@@ -10,7 +10,7 @@ const magnet = "magnet:?xt=urn:btih:c9e15763f722f23e98a29decdfae341b98d53056&dn=
 
 func TestParseOfInvalidUri(t *testing.T){
 	is := is.New(t)
-	uri := strings.ReplaceAll(magnet, "magnet:?", "")
+	uri := strings.Replace(magnet, "magnet:?", "", 1)
 	err, _ := Parse(uri)
 
 	is.True(err != nil)
@@ -28,17 +28,18 @@ func TestParseMagnetUri(t *testing.T) {
 	is.Equal(tor.Origin, magnet)
 	is.Equal(tor.Xt, "urn:btih:c9e15763f722f23e98a29decdfae341b98d53056")
 	is.Equal(tor.DisplayName, "Cosmos Laundromat")
-	is.Equal(tor.Hash, "c9e15763f722f23e98a29decdfae341b98d53056")
-	// xs == https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fcosmos-laundromat.torrent
+	is.Equal(tor.Source, "https://webtorrent.io/torrents/cosmos-laundromat.torrent")
 
-	is.True(len(tor.Addr) == 8)
-	is.Equal(tor.Addr[0], "udp://explodie.org:6969")
-	is.Equal(tor.Addr[1], "udp://tracker.coppersurfer.tk:6969")
-	is.Equal(tor.Addr[2], "udp://tracker.empire-js.us:1337")
-	is.Equal(tor.Addr[3], "udp://tracker.leechers-paradise.org:6969")
-	is.Equal(tor.Addr[4], "udp://tracker.opentrackr.org:1337")
-	is.Equal(tor.Addr[5], "wss://tracker.btorrent.xyz")
-	is.Equal(tor.Addr[6], "wss://tracker.fastcast.nz")
-	is.Equal(tor.Addr[7], "wss://tracker.openwebtorrent.com")
+	is.Equal(len(tor.Trackers), 8)
+	is.Equal(tor.Trackers[0], "udp://explodie.org:6969")
+	is.Equal(tor.Trackers[1], "udp://tracker.coppersurfer.tk:6969")
+	is.Equal(tor.Trackers[2], "udp://tracker.empire-js.us:1337")
+	is.Equal(tor.Trackers[3], "udp://tracker.leechers-paradise.org:6969")
+	is.Equal(tor.Trackers[4], "udp://tracker.opentrackr.org:1337")
+	is.Equal(tor.Trackers[5], "wss://tracker.btorrent.xyz")
+	is.Equal(tor.Trackers[6], "wss://tracker.fastcast.nz")
+	is.Equal(tor.Trackers[7], "wss://tracker.openwebtorrent.com")
+
+	is.Equal(tor.Hash, "c9e15763f722f23e98a29decdfae341b98d53056")
 
 }
